60d5ca99367910b493a4a97ac51c188ac4abfff7
Rhys Rushton <rhys.rushton@gmail.com>
2020-05-04 13:49:34 +1000

    search button

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+//Query URLS
+var currentWeatherSearch = "https://api.openweathermap.org/data/2.5/weather?q=" ;
+var forecastweatherSearch = "https://api.openweathermap.org/data/2.5/forecast?q=" ;
+//var uvSearch =  "https://api.openweathermap.org/data/2.5/uvi?"; 
+
+//API Key
+var api = "249525d4c4a0c98c46b32b44ba6865f2"; 
+
+
+//Search 
+$("searchBn").on("click", console.log("Click"))
