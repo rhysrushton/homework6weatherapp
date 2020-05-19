fb009e23b933999860c4eb58687daf811ae5cb09
Rhys Rushton <rhys.rushton@gmail.com>
2020-05-19 10:34:56 +1000

    humidity and wind done

--- javascript/script.js
+++ javascript/script.js
@@ -44,8 +44,8 @@ function search(){
     );
     $("#dateAndTime").text("Local Time & Date: " + dateAndTime);
     $("#temp").text("The current temperature in" + `${recentSearch.city}` + "is" + response.main.temp.toFixed(0) + "C");
-
-
+    $("#humidity").text("The Humdity is:" + response.main.humidity + "%");
+    $("#wind").text("The Wind speed is: " + response.wind.speed + "m/sec");
   })
 
 
