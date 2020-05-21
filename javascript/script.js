fa3f02bfdb35badabddfde1e15945afb7bef82f5
Rhys Rushton <rhys.rushton@gmail.com>
2020-05-21 10:50:23 +1000

    5 day html built with jquery

--- javascript/script.js
+++ javascript/script.js
@@ -54,6 +54,51 @@ var requestForecast = $.ajax({
 }) 
 requestForecast.done(function(call){
   console.log(call)
+
+ 
+}).then(function(response){
+  var dateArray = [];
+  var iconArray = [];
+  var tempArray = [];
+  var humidityArray = []; 
+  for(var i=1; i < 40; i++){
+    var option = response.list[i].dt_txt.substring(11);
+   // console.log(option)
+    var dateValue = response.list[i].dt_txt.substring(0,10);
+   // console.log(dateValue);
+    var currentDate = moment().format("ddd, MMM Do HH:mm");
+   // console.log(currentDate);
+    if("15:00:00" == option && dateValue != currentDate){
+      var dateString = response.list[i].dt_txt.substring("0,10");
+      var date = new moment(dateString)
+      var formatDate = date.format("MM/DD/YYYY")
+      dateArray.push(formatDate);
+      iconArray.push(response.list[i].weather[0].icon);
+      tempArray.push(response.list[i].main.temp);
+      humidityArray.push(response.list[i].main.humidity);
+console.log(humidityArray)
+console.log(iconArray)
+console.log(tempArray)
+console.log(dateArray)
+    };
+ 
+  };
+
+
+  var newHumidity = $("<p>").html("Humidity:");
+  $(".forecast").append(newHumidity); 
+
+  var newTemp = $("<p>").html("Temperature:");
+  $(".forecast").append(newTemp);  
+
+  var newImg = $("<p>").html("Icon:");
+  $(".forecast").append(newImg); 
+
+  var newDate = $("<p>").html("Date:");
+  $(".forecast").append(newDate);  
+ 
+
+
 })
 
 
